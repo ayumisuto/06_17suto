cf5457f12ea2982244b40f2ec3084e28df43ab67
ayumisuto <54233849+ayumisuto@users.noreply.github.com>
2019-09-11 19:11:26 +0900

    Update Word.java

--- ColLesson02/Word.java
+++ ColLesson02/Word.java
@@ -4,12 +4,13 @@ public class Word {
     String word;
     String meaning;
 
-    public String toString(){
+    public String toString() {
+    return "単語:" + this.word + "意味:" + this.meaning;
+    }
 
+    public Word(String input){
         String[] str = input.split(" ");
-        //↑　いらない？
         this.word = str[0];
         this.meaning = str[1];
-    return "単語:" + this.word + "意味:" + this.meaning;
-}
+    }
 }
